78899556cee194170935398d5f156cb6e37707f1
Rafael <rafaelreinertd@gmail.com>
2023-01-06 05:45:40 -0300

    Final

--- js/javascript.js
+++ js/javascript.js
@@ -40,11 +40,9 @@ function finalizar(){
     document.querySelector('.final-bebida p:last-of-type').innerHTML=precoBebida.toString(10).replace('.',',');
     document.querySelector('.final-sobremesa p:first-of-type').innerHTML=nomeSobremesa;
     document.querySelector('.final-sobremesa p:last-of-type').innerHTML=precoSobremesa.toString(10).replace('.',',');
-    const precoTotal = (Number(precoPrato)+Number(precoBebida)+Number(precoSobremesa)).toString(10).slice(0,5).replace('.',',');
+    const precoTotal = (Number(precoPrato)+Number(precoBebida)+Number(precoSobremesa)).toFixed(2).toString(10).replace('.',',');
     document.querySelector('.final-total p:last-of-type').innerHTML=`R$ ${precoTotal}`;
 
-    //const msg = `https://api.whatsapp.com/send?phone=5547999779295&text=Ol%C3%A1%2C%20gostaria%20de%20fazer%20o%20pedido%3A%0A-%20Prato%3A%20${nomePrato}%0A-%20Bebida%3A%20${nomeBebida}%0A-%20Sobremesa%3A%20${nomeSobremesa}%0ATotal%3A%20R%24%20${precoTotal}%0A
-    //%0ANome%3A%20${nome}%0AEndere%C3%A7o%3A%20${endereco}`;
     const msg = `https://api.whatsapp.com/send?phone=5547999779295&text=${encodeURIComponent(`Olá, gostaria de fazer o pedido: `)}%0A
 ${encodeURIComponent(`- Prato: ${nomePrato}`)}%0A${encodeURIComponent(`- Bebida: ${nomeBebida}`)}%0A${encodeURIComponent(`- Sobremesa: ${nomeSobremesa}`)}%0A
 ${encodeURIComponent(`Total: R$ ${precoTotal}`)}%0A%0A${encodeURIComponent(`Nome: ${nome}`)}%0A${encodeURIComponent(`Endereço: ${endereco}`)}`;
